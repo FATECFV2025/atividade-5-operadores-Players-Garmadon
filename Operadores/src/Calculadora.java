302a0edc811be2e89c063006c998f282b495f7b9
Players-Garmadon <guilherme.almeida81@fatec.sp.gov.br>
2025-11-07 11:44:15 -0300

    Criando, alterando e deletando os arquivos necessários para a atividade

---
+++ Operadores/src/Calculadora.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Calculadora
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+
+        s.close();
+    }
+}
--- Operadores/src/teste.java
+++
@@ -1,4 +0,0 @@
-public class teste
-{
-    public int atributo1;
-}
