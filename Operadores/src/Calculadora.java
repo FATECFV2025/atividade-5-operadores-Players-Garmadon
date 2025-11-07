86424d0d2e9a4724d2f7ec126cfc5f02e909c21a
Players-Garmadon <guilherme.almeida81@fatec.sp.gov.br>
2025-11-07 12:06:07 -0300

    Criando umando de input do valor a ser operado

--- Operadores/src/Calculadora.java
+++ Operadores/src/Calculadora.java
@@ -4,12 +4,20 @@ public class Calculadora
 {
     public static void main(String[] args)
     {
-        /*Scanner s = new Scanner(System.in);
-
-        s.close();*/
+        Scanner s = new Scanner(System.in);
 
         Operadores op = new Operadores();
 
-        System.out.println("Aaa: " + op.adicao(1, 2));
+        System.out.print("Digite um número: ");
+        float n1 = s.nextFloat();
+        System.out.print("Digite um outro número: ");
+        float n2 = s.nextFloat();
+
+        op.v1 = n1;
+        op.v2 = n2;
+
+        System.out.println("Aaa: " + op.adicao(n1, n2));
+
+        s.close();
     }
 }
